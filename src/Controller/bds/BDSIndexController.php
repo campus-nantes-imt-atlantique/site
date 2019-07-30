c36bd3310fc0480c9ea075309b6775b2fd1494a7
Wathis <delaunaymathis@yahoo.fr>
2019-07-30 19:39:05 +0200

    Leaders

--- src/Controller/bds/BDSIndexController.php
+++ src/Controller/bds/BDSIndexController.php
@@ -25,8 +25,41 @@ class BDSIndexController extends AbstractController
      */
     public function leaders()
     {
+        $leaders = array(
+            "Athlétisme relais" => ["Valentin Jacques"],
+            "Athlétisme cross" => ["Adrien Delbroeuve"],
+            "Badminton" => ["Adrien Delbroeuve"],
+            "Basket F" => ["Miora Frossard" , "Clarisse Lê"],
+            "Basket M" => ["Clément André" , " Abdessamad Ghilane"],
+            "Danse" => ["Anna Rogova"],
+            "Equitation" => ["?"],
+            "Escalade" => ["Ismaël Benkirane"],
+            "Fitness" => ["Anna Rogova"],
+            "Foot F" => ["Manon Geniesse" , "Maeliss Guibert"],
+            "Foot M" => ["Nicolas Chauveau" , "Ilias Amal"],
+            "Golf" => ["Stanislas Roger" , "Mathieu Cazes"],
+            "Hand F" => ["Elsa Fourneau"],
+            "Hand M" => ["Luc Cerisy" , "Arthur Chauvin"],
+            "Hip-Hop" => ["?"],
+            "Boxe" => ["Thomas Herbelin"],
+            "Natation " => ["Mathieu Cazes"],
+            "Pompom" => ["Pauline Lery" , "Caroline de Pourtales"],
+            "Rugby F" => ["Albane de Verdière" , "Léa Louesdon"],
+            "Rugby M" => ["Maxime Zeidenberg" , "Alexandre Naudi"],
+            "Tennis" => ["Baptiste Guimard"],
+            "Volley F" => ["Jeanne Thévenin"],
+            "Volley M" => ["Romain Goldsztajn" , "Kevin Thiebaut"],
+            "Musculation" => ["Quentin Marinelli"],
+            "Tennis de table" => ["Valentin Jacques"],
+            "Escrime" => ["Maximilien Bertaux"],
+            "Aviron" => ["Pierre Malécot ?"],
+            "Voile" => ["?"],
+            "Ultimate" => ["Alexis Schneider,Valentin Jacques"],
+            "VTT/Raid" => ["?"]
+        );
         return $this->render('bds/leaders.html.twig', [
             'controller_name' => 'BDSIndexController',
+            'leaders' => $leaders
         ]);
     }
 
