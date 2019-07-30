7f1b60914ebf8d251df2f3bffac48505e85f309d
mathis.delaunay <delaunaymathis@yahoo.fr>
2019-07-30 22:00:36 +0200

    feat: Security bundle and EasyAdmin Bundle

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'root'
+        ));
+
+        $user->setRoles(["ROLE_ADMIN"]);
+
+        $user->setEmail('root@imt.fr');
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
