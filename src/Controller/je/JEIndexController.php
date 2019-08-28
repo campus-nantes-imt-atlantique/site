69fce118f9a251ea8210eb75faaae5010202a961
Oussama <chaib.ous.tfk@gmail.com>
2019-08-28 15:52:52 +0200

    Les grandes lignes et la structure de la page JE. Des changements dans translation/messages.fr.yml :    -ajout de la partie je, je.je_title, je.je_description je.missions et je.nav    -changement de l'index de la section je (ajout de du nav de la page)

--- src/Controller/je/JEIndexController.php
+++ src/Controller/je/JEIndexController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\je;
 
+
+use App\Entity\Content;
+use App\Entity\Member;
+use App\Entity\Pole;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class JEIndexController extends AbstractController
@@ -10,10 +15,46 @@ class JEIndexController extends AbstractController
     /**
      * @Route("/je", name="je_index")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $description = $this->getDoctrine()->getRepository(Content::class)->findContentByKeyAndLang("description_je", $request->getLocale());
+        $poles = $this->getDoctrine()->getRepository(Pole::class)->findBySectionName('JE');
+
         return $this->render('je/index.html.twig', [
             'controller_name' => 'JEIndexController',
+            'description' => $description,
+            'poles' => $poles
+        ]);
+    }
+
+    /**
+     * @Route("/je/etude", name="je_etude")
+     */
+    public function etude()
+    {
+        return $this->render('je/deroulement_etude.html.twig',[
+            'controller_name' => 'JEIndexController'
+        ]);
+    }
+
+    /**
+     * @Route("je/contact",name="je_contact")
+     */
+    public function contact()
+    {
+        return $this->render('je/contact.html.twig',[
+            'controller_name' => 'JEIndexController'
+        ]);
+    }
+
+    /**
+     * @Route("je/missions",name="je_missions")
+     */
+    public function missions()
+    {
+        return $this->render('je/missions.html.twig',[
+            'controller_name' => 'JEIndexController'
         ]);
     }
 }
+
