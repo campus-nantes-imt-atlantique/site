42d63209e1a39a22f82e0a6b2dc36b37b44131bd
Wathis <delaunaymathis@yahoo.fr>
2019-10-19 16:45:46 +0200

    audit

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    DataDog\AuditBundle\DataDogAuditBundle::class => ['all' => true],
 ];
