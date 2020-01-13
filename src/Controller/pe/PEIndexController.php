27941e146eeaaa7cdce32d28566c6c7a67ff2d66
kevin-lth <kevin.lethuillier@gmail.com>
2020-01-13 16:38:43 +0100

    Redirect when trying to read invalid edition Fixed URLs for reading

--- src/Controller/pe/PEIndexController.php
+++ src/Controller/pe/PEIndexController.php
@@ -41,18 +41,22 @@ class PEIndexController extends AbstractController
 
     /**
      * @Route({
-     *     "en": "/pe/lire/{edition}",
-     *     "fr": "/pe/read/{edition}"
-     * }, name="pe_read", requirements={"page"="{\d+:\d+\}"})
+     *     "en": "/pe/read/{edition}",
+     *     "fr": "/pe/lire/{edition}"
+     * }, name="pe_read", requirements={"edition"="\d+"})
      */
     // L'édition doit correspondre à un nombre
     public function read(Request $request, int $edition)
     {
         $journal = $this->getDoctrine()->getRepository(Journal::class)->findOneBy(['edition' => $edition]);
-        return $this->render('pe/read.html.twig', [
-            'controller_name' => 'PEIndexController',
-            "navigation_description" => $this->getDoctrine()->getRepository(Content::class)->findContentByKeyAndLang("navigation_description","PE", $request->getLocale()),
-            "journal" => $journal
-        ]);
+        if (!is_null($journal) and !is_null($journal->getFile()) and $journal->getFile() != '') {
+            return $this->render('pe/read.html.twig', [
+                'controller_name' => 'PEIndexController',
+                "navigation_description" => $this->getDoctrine()->getRepository(Content::class)->findContentByKeyAndLang("navigation_description","PE", $request->getLocale()),
+                "journal" => $journal
+            ]);
+        } else {
+            return $this->redirectToRoute('pe_list');
+        }
     }
 }
